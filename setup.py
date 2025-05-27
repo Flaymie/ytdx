7b3548ad40ab21641f02272dec2ca8265e187295
Vladimir Burenko <funquenop@gmail.com>
2025-05-27 08:09:42 +0500

    translate eng

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ytdx",
-    version="0.1.0",
+    version="1.0.0",
     description="YouTube video downloader via command line",
     author="Flaymie",
     author_email="funquenop@gmail.com",
